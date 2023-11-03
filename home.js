4aa7692e9a7c130f04b532dbe6bc33951cb44c6a
Ayolan8 <122812770+Ayolan8@users.noreply.github.com>
2023-11-03 20:18:44 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+        //open the dropdown menu from the nav bar
+        function openBtn() {
+            const open = document.getElementById("dpContent");
+    
+            open.style.width = "230px"
+            
+           }
+           //close the dropdown menu
+           function closeBtn() {
+            const close = document.getElementById("dpContent");
+    
+            close.style.width = "0";
+           }
